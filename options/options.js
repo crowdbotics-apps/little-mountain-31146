958eb16d488b5569ff77d00e6fbfa8082ca6922c
Crowdbotics <team@crowdbotics.com>
2021-10-06 03:42:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "little_mountain_31146",
+  url: "https://little_mountain_31146.botics.co",
+  api: "https://little_mountain_31146.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
